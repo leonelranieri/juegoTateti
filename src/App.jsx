6e8e6e3513405b12dcea4ee97cb88d45fb7e6926
LRanieri <leonelranieri0501@hotmail.com>
2023-11-19 20:09:34 -0300

    botón restaurar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 // Componente Square: representa un cuadrado en el tablero
-function Square({ value, onSquareClick, isWinnerSquare  }) {
+function Square({ value, onSquareClick, isWinnerSquare }) {
   // Clases y estilos dinámicos basados en el valor del cuadrado (X, O)
   let squareClass = "square";
   let squareStyle = {};
@@ -21,7 +21,7 @@ function Square({ value, onSquareClick, isWinnerSquare  }) {
   }
   // Renderiza un botón con el valor del cuadrado y estilos dinámicos
   return (
-    <button className={squareClass} style={squareStyle} onClick={onSquareClick}>
+    <button className={squareClass} style={squareStyle} onClick={() => {onSquareClick(); onHighlightSquare(position);}}>
       {value}
     </button>
   );
@@ -29,6 +29,10 @@ function Square({ value, onSquareClick, isWinnerSquare  }) {
 
 // Componente Board: representa el tablero del juego
 function Board({ xIsNext, squares, onPlay }) {
+  const winnerInfo = calculateWinner(squares);
+  const winner = winnerInfo ? winnerInfo.winner : null;
+  const winningLine = winnerInfo ? winnerInfo.line : null;
+
   // Maneja el clic en un cuadrado y actualiza el estado del juego
   function handleClick(i) {
     // Verifica si hay un ganador o si el cuadrado ya está ocupado
@@ -48,7 +52,6 @@ function Board({ xIsNext, squares, onPlay }) {
   }
 
   // Calcula al ganador y muestra el estado actual del juego
-  const winner = calculateWinner(squares);
   let status;
   // Si hay un ganador, muestra el mensaje de ganador
   if (winner) {
@@ -58,24 +61,39 @@ function Board({ xIsNext, squares, onPlay }) {
     status = 'Siguiente jugador: ' + (xIsNext ? 'X' : 'O');
   }
 
+  // Función para renderizar cada cuadrado con la información de la línea ganadora
+  function renderSquare(i) {
+    const isWinnerSquare = winningLine && winningLine.includes(i);
+    return (
+      <Square
+        value={squares[i]}
+        onSquareClick={() => handleClick(i)}
+        isWinnerSquare={isWinnerSquare}
+        //position={i} // Pasa la posición del cuadrado
+        //onHighlightSquare={(position) => highlightSquare(position)}
+        key={i}
+      />
+    );
+  }
+
   // Renderiza el tablero con sus cuadrados
   return (
     <>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </>
   );
@@ -86,6 +104,8 @@ export default function Game() {
   // Estado para el historial de movimientos y el movimiento actual
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [resetCounter, setResetCounter] = useState(0); // Contador de restablecimientos
+  //const [highlightedSquare, setHighlightedSquare] = useState(null); // Cuadrado resaltado
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -96,6 +116,8 @@ export default function Game() {
     // Agrega el nuevo estado al historial y actualiza el movimiento actual
     setHistory([...nextHistory, nextSquares]);
     setCurrentMove(nextHistory.length);
+    // Resetea el cuadrado resaltado al realizar un nuevo movimiento
+    //setHighlightedSquare(null);
   }
 
   // Cambia al estado de juego de un movimiento específico
@@ -103,9 +125,27 @@ export default function Game() {
     // Establece el movimiento actual y corta el historial hasta ese punto
     setCurrentMove(nextMove);
     setHistory(history.slice(0, nextMove + 1));
+    // Resetea el cuadrado resaltado al retroceder a un movimiento anterior
+    //setHighlightedSquare(null);
   }
 
-  // Genera la lista de movimientos anteriores
+  // Función para restablecer el juego
+  function handleReset() {
+    // Incrementa el contador de restablecimiento
+    setResetCounter(resetCounter + 1);
+    // Restablece el historial y el movimiento actual al estado inicial
+    setHistory([Array(9).fill(null)]);
+    setCurrentMove(0);
+    // Resetea el cuadrado resaltado al reiniciar el juego
+    //setHighlightedSquare(null);
+  }
+
+  // Función para resaltar el cuadrado correspondiente a la jugada
+  // function highlightSquare(index) {
+  //   setHighlightedSquare(index);
+  // }
+
+  // Genera la lista de movimientos anteriores y botones para resaltar cuadrados
   const moves = history.map((squares, move) => {
     let description;
     // Define la descripción del movimiento actual
@@ -116,7 +156,10 @@ export default function Game() {
           {move === currentMove ? (
             <span>Movimiento actual # {move}</span>
           ) : (
-            <button onClick={() => jumpTo(move)}>{description}</button>
+            <>
+              <button onClick={() => jumpTo(move)}>{description}</button>
+              {/* <button onClick={() => highlightSquare(move)}>Mostrar ubicación</button> */}
+            </>
           )}
         </li>
       );
@@ -138,15 +181,23 @@ export default function Game() {
     <div className="game">
       <div className="game-board">
         <h1>TA-TE-TI</h1>
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <button className='reset' onClick={handleReset}>Restablecer</button>
+        <Board
+          xIsNext={xIsNext}
+          squares={currentSquares}
+          onPlay={handlePlay}
+          // onHighlightSquare={highlightedSquare}
+        />
       </div>
       <div className="game-info">
+        <div>{statusMessage}</div>
         <ol>{moves}</ol>
       </div>
     </div>
   );
 }
 
+// ---------------- FUNCTIONS -----------------------
 // Función calculateWinner: determina al ganador del juego
 function calculateWinner(squares) {
   const lines = [
@@ -163,7 +214,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return { winner: squares[a], line: lines[i] };
     }
   }
   // Si no hay ganador, devuelve null
